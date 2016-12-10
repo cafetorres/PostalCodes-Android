cd60cbb000db24b666e9271f13196d960ad1fca1
Carlos Torres <cf.torresluna@gmail.com>
2016-12-10 02:37:05 -0600

    se agrego la vista de detalles

--- app/src/main/java/me/cafetorres/cp_android/Details_cp.java
+++ app/src/main/java/me/cafetorres/cp_android/Details_cp.java
@@ -1,10 +1,37 @@
 package me.cafetorres.cp_android;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Carlos on 10/12/2016.
  */
 
 public class Details_cp extends AppCompatActivity {
+    @Bind(R.id.placename)
+    TextView placename;
+    @Bind(R.id.postalcode)
+    TextView postalcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+
+
+
+
+
+        placename.setText("Place Name: " + intent.getStringExtra("placename"));
+        postalcode.setText("Postal Code: " + intent.getStringExtra("postalcode"));
+
+    }
 }
